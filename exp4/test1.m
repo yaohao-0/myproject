1f7c1e45599259a221afe6702cfb7b78eee1cbb9
yaohao-0 <1084192483@qq.com>
2017-11-29 00:00:35 +0800

    实验四

--- exp4/test1.m
+++ exp4/test1.m
@@ -11,9 +11,9 @@ for k=1:200
     end
     image=imread([path filename]);%读取图像
     [M,N]=size(image);
-    image_th=(image>196);
-    S=sum(sum(image_th(:,1:N-10)==0));% 计算面积
-    % 计算形心      
+    image_th=(image>196);%阈值分割
+    S=sum(sum(image_th(:,1:N-10)==0));%计算面积
+    %计算形心      
     x=0;y=0;
     for i=1:M
         for j=1:N-10
@@ -26,7 +26,7 @@ for k=1:200
     imshow(image);
     hold on
     plot(xc,yc,'r+','markersize',3);%画出形心位置
-    rectangle('Position',[xc-25,yc-8,64,16]);%画出矩形框位置
+    rectangle('Position',[xc-25,yc-8,64,16],'EdgeColor','r');%画出矩形框位置
     title(['Frame NO. ',num2str(k)]);
     pause(0.01)
     hold off
--- exp4/test2.m
+++ exp4/test2.m
@@ -2,6 +2,8 @@ clc,close all,clear all;
 path='f16takeoff_396s/';
 I=zeros(240,360,396);
 [M, N, P]=size(I);
+%% 读取图像
+fprintf('Reading the images...\n');
 for k=1:P
     name=num2str(k);
     if k<=9
@@ -14,25 +16,50 @@ for k=1:P
     image=imread([path filename]);%读取图像
     I(:,:,k)=double(rgb2gray(image));
 end
-wx=90;wy=130;lx=40;ly=90;%模板位置与大小
+fprintf('DONE!press Enter to the next step\n');
+pause
+%% 处理图像
+clc
+wx=zeros(P,1);wy=zeros(P,1);
+wx(1)=90;wy(1)=130;lx=40;ly=90;%模板位置与大小
 Dx=10;Dy=10;%搜索范围
 for k=1:P-1
+    fprintf('Processing the NO.%d image\n',k);
     MAD=zeros(2*Dx+1,2*Dy+1);
     for dx=-Dx:Dx
         for dy=-Dy:Dy
-            for i=wx:wx+lx
-                for j=wy:wy+ly
+            for i=wx(k):wx(k)+lx
+                for j=wy(k):wy(k)+ly
                     MAD(dx+Dx+1,dy+Dy+1)=MAD(dx+Dx+1,dy+Dy+1)+abs(I(i,j,k)-I(i+dx,j+dy,k+1));
                 end
             end
         end
     end
-    [x,y]=find(MAD==min(min(MAD)));
-    imshow(I(:,:,k),[]);
+    MAD_min=min(min(MAD));
+%     mesh(MAD)
+%     title(['Frame NO. ',num2str(k)]);
+%     pause(0.01)
+    [x,y]=find(MAD==MAD_min);
+    wx(k+1)=wx(k)+x-Dx-1;wy(k+1)=wy(k)+y-Dy-1;
+    clc
+end
+%% 显示结果
+fprintf('DONE!press Enter to show the result\n');
+pause  
+for k=1:P
+    name=num2str(k);
+    if k<=9
+        filename=strcat('00',name,'.jpg');
+    elseif k<=99
+        filename=strcat('0',name,'.jpg');
+    else
+        filename=strcat(name,'.jpg');
+    end
+    image=imread([path filename]);%读取图像
+    imshow(image);
     hold on
-    rectangle('Position',[wy,wx,ly,lx]);
+    rectangle('Position',[wy(k),wx(k),ly,lx],'EdgeColor','r');
     title(['Frame NO. ',num2str(k)]);
-    pause(0.2)
+    pause(0.01)
     hold off
-    wx=wx+x-Dx-1;wy=wy+y-Dy-1;
 end
