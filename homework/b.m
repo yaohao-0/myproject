36cad3dda7322b59031aa714bb368e5664b0ce50
yaohao-0 <1084192483@qq.com>
2017-12-03 20:55:14 +0800

    1

--- homework/b.m
+++ homework/b.m
@@ -1,12 +1,13 @@
-%彩色图像运动模糊
-clear,clc,close all;
-image=imread('art.jpg');
-R=image(:,:,1);G=image(:,:,2);B=image(:,:,3);
-f=fspecial('motion',50,45);
-blurred_R=imfilter(R,f,'conv','replicate','same');
-blurred_G=imfilter(G,f,'conv','replicate','same');
-blurred_B=imfilter(B,f,'conv','replicate','same');
-blurred_image(:,:,1)=blurred_R;
-blurred_image(:,:,2)=blurred_G;
-blurred_image(:,:,3)=blurred_B;
-imshow(blurred_image);
+clc,clear all,close all;
+I=imread('tank.jpg');
+I=double(I);
+[M,N]=size(I);
+I_max=max(max(I));I_min=min(min(I));
+I_new=zeros(M,N);
+for i=1:M
+    for j=1:N
+        I_new(i,j)=(I(i,j)-I_min)/(I_max-I_min);
+    end
+end
+subplot(1,2,1),imshow(uint8(I)),title('Original');
+subplot(1,2,2),imshow(I_new),title('Normalized');
